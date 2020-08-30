71722e12b1f0b36c5354237e2c3f930fc31e67cd
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-30 19:10:07 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,23 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  new_hash = {}
+  new_hash[key] = value
+  return new_hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
+  if hash[key]
+    hash[key] += 1
+  else
+    hash[key] = 1
+  end
+  return hash
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
+
+puts update_counting_hash(my_favorite_stream = { name: 7, location: 7 }, :number)
