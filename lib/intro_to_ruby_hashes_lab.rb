b95aa96ace9cbe8aa7e27c5da9e451ff62a7996a
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-30 18:59:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,25 @@
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  new_hash = Hash.new
+  new_hash[:key] = value
+  return new_hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
+  if hash[key] in hash
+    hash[key] += 1
+  else
+    hash[key] = 1
+  end
+  updated_hash = hash
+  return updated_hash
+end
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
+
+puts update_counting_hash(hash_to_read = {location: 7, occupation: 5}, :occupation)
